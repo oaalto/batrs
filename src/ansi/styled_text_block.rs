2d24a055eb04e837ed80d08f615e2416235ebe3f
Olli Aalto <oaalto@gmail.com>
2024-07-18 11:27:24 +0300

    Remove unused function

--- src/ansi/styled_text_block.rs
+++ src/ansi/styled_text_block.rs
@@ -9,14 +9,6 @@ pub struct StyledChar {
 }
 
 impl StyledChar {
-    pub fn new(c: String) -> Self {
-        Self {
-            bold: false,
-            color: AnsiCode::White,
-            character: c,
-        }
-    }
-
     pub fn reset(&mut self) {
         self.bold = false;
         self.color = AnsiCode::White;
